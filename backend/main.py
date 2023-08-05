4e4b6ad02c6fdf3fee46be0f8c401d3d9f718283
Rosheen C <rosheenc123@gmail.com>
2023-08-05 12:28:19 -0700

    testing deploy

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from models import User, SessionLocal
+# from models import User #, SessionLocal
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -9,7 +9,6 @@ app = FastAPI()
 origins = [
 '*'
 ]
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,31 +23,39 @@ class UserResponse(BaseModel):
     username: str
     email: str
 
-# Endpoint to fetch all users
+@app.get("/", response_model=None)
+def root(limit: int = 10):
+    return {"hello": "world"}
+
 @app.get("/users", response_model=None)
 def get_all_users(limit: int = 10):
-    db = SessionLocal()
-    try:
-        users = db.query(User).limit(limit).all()
-        return users
-    finally:
-        db.close()
+    return {"hello": "world"}
 
-# Endpoint to create a new user
-@app.post("/users", response_model=UserResponse)
-def create_user(username: str, email: str):
-    db = SessionLocal()
-    try:
-        # Check if the user already exists
-        existing_user = db.query(User).filter(User.username == username).first()
-        if existing_user:
-            raise HTTPException(status_code=400, detail="Username already exists")
+# Endpoint to fetch all users
+# @app.get("/users", response_model=None)
+# def get_all_users(limit: int = 10):
+#     db = SessionLocal()
+#     try:
+#         users = db.query(User).limit(limit).all()
+#         return users
+#     finally:
+#         db.close()
 
-        new_user = User(username=username, email=email)
-        db.add(new_user)
-        db.commit()
-        db.refresh(new_user)  # Refresh the new_user object to get the updated ID after committing
+# # Endpoint to create a new user
+# @app.post("/users", response_model=UserResponse)
+# def create_user(username: str, email: str):
+#     db = SessionLocal()
+#     try:
+#         # Check if the user already exists
+#         existing_user = db.query(User).filter(User.username == username).first()
+#         if existing_user:
+#             raise HTTPException(status_code=400, detail="Username already exists")
 
-        return UserResponse(id=new_user.id, username=new_user.username, email=new_user.email)
-    finally:
-        db.close()
+#         new_user = User(username=username, email=email)
+#         db.add(new_user)
+#         db.commit()
+#         db.refresh(new_user)  # Refresh the new_user object to get the updated ID after committing
+
+#         return UserResponse(id=new_user.id, username=new_user.username, email=new_user.email)
+#     finally:
+#         db.close()
