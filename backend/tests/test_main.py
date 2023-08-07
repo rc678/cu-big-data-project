f7ce58f037555ae5ba64a82e2b90701e7b8f7433
Rosheen C <rosheenc123@gmail.com>
2023-08-06 20:15:32 -0700

    cleanup test path

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -4,7 +4,8 @@ from main import app
 
 client = TestClient(app)
 
-
+# How to run test: python -m pytest tests
+# https://stackoverflow.com/questions/20985157/py-test-no-module-named
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
